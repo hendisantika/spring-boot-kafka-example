d51c3a8bd0365ff930e6a456edae7f5da276e188
Hendi Santika <hendisantika@gmail.com>
2021-12-20 19:13:31 +0700

    #6 - Add producerConfigs in KafkaConfig

--- src/main/java/com/hendisantika/KafkaConfig.java
+++ src/main/java/com/hendisantika/KafkaConfig.java
@@ -1,9 +1,16 @@
 package com.hendisantika;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +28,13 @@ public class KafkaConfig {
 
     @Value("${hendi.topic-name}")
     private String topicName;
+
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props =
+                new HashMap<>(kafkaProperties.buildProducerProperties());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return props;
+    }
 }
