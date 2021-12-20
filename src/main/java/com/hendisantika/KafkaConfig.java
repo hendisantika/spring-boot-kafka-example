207592bc7801accdb76330b60119377fe87cb6f9
Hendi Santika <hendisantika@gmail.com>
2021-12-20 19:14:11 +0700

    #8 - Add kafkaTemplate in KafkaConfig

--- src/main/java/com/hendisantika/KafkaConfig.java
+++ src/main/java/com/hendisantika/KafkaConfig.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
@@ -43,5 +44,10 @@ public class KafkaConfig {
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
         return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 }
