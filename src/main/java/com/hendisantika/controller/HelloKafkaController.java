fd83b0e3405ced2346b8e457e8b21e180e9c5887
Hendi Santika <hendisantika@gmail.com>
2021-12-20 19:19:16 +0700

    #17 - Add hello in HelloKafkaController

--- src/main/java/com/hendisantika/controller/HelloKafkaController.java
+++ src/main/java/com/hendisantika/controller/HelloKafkaController.java
@@ -1,12 +1,16 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.PracticalAdvice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 /**
  * Created by IntelliJ IDEA.
@@ -34,5 +38,17 @@ public class HelloKafkaController {
         this.template = template;
         this.topicName = topicName;
         this.messagesPerRequest = messagesPerRequest;
+    }
+
+    @GetMapping("/hello")
+    public String hello() throws Exception {
+        latch = new CountDownLatch(messagesPerRequest);
+        IntStream.range(0, messagesPerRequest)
+                .forEach(i -> this.template.send(topicName, String.valueOf(i),
+                        new PracticalAdvice("A Practical Advice", i))
+                );
+        latch.await(60, TimeUnit.SECONDS);
+        logger.info("All messages received");
+        return "Hello Kafka!";
     }
 }
